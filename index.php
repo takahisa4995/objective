a905c64b0963af6efa0a58f89098ed6efc5207a6
髙久 元希 <takaku.genki@LUANDA.local>
2019-10-03 09:34:46 +0900

    非ログイン状態でも投稿一覧を見れるように修正

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+ini_set('log_errors','on'); // ログを取るか
+ini_set('error_log','php_error.log'); //ログファイルの出力先
+
+// エネミー格納用
+$enemy = array();
+
+// 性別クラス
+class Jender{
+    const MAN = 1;
+    
+}
+
+?>
