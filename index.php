20ed58456c92e2537e92abb2b7315ac20e41cfa8
髙久 元希 <takaku.genki@LUANDA.local>
2019-10-03 16:57:16 +0900

    関数と定型文を別ファイルに切り分け

--- index.php
+++ index.php
@@ -1,30 +1,9 @@
 <?php
 
-ini_set('log_errors','on'); // ログを取るか
-ini_set('error_log','php_error.log'); //ログファイルの出力先
-session_start();
+require 'function.php';
+require 'message.php';
 
-$debug_flg = true;
-function debug($str){
-  global $debug_flg;
-  if($debug_flg) error_log($str);
-}
 
-//定型文(セリフ)
-define('MSG01', 'のクリティカルヒット！！');
-define('MSG02','ポイントのダメージ！！');
-define('MSG03','が叫ぶ！');
-define('MSG04','ぐはっ！');
-define('MSG05','きゃっ！');
-define('MSG06','...');
-define('MSG07','はうっ！');
-define('MSG08','の魔法攻撃！！');
-define('MSG09','が現れた！！');
-define('MSG10','リスタート');
-define('MSG11', 'ゲームスタート！！');
-define('MSG12','の攻撃！');
-define('MSG13','を撃破！');
-define('MSG14', '逃げた！');
 
 
 
---
+++ message.php
@@ -0,0 +1,17 @@
+<?php
+//定型文(セリフ)
+define('MSG01', 'のクリティカルヒット！！');
+define('MSG02','ポイントのダメージ！！');
+define('MSG03','が叫ぶ！');
+define('MSG04','ぐはっ！');
+define('MSG05','きゃっ！');
+define('MSG06','...');
+define('MSG07','はうっ！');
+define('MSG08','の魔法攻撃！！');
+define('MSG09','が現れた！！');
+define('MSG10','リスタート');
+define('MSG11', 'ゲームスタート！！');
+define('MSG12','の攻撃！');
+define('MSG13','を撃破！');
+define('MSG14', '逃げた！');
+?>
